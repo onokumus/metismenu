1ca2e7e72888df7f8d3f26f15789bed4283a41a9
tnguyenle <thinh1011@gmail.com>
2015-10-22 11:44:42 +0700

    add option *iconSelect*

--- dist/metisMenu.js
+++ dist/metisMenu.js
@@ -80,7 +80,8 @@
     collapseInClass: 'in',
     collapsingClass: 'collapsing',
     onTransitionStart: false,
-    onTransitionEnd: false
+    onTransitionEnd: false,
+    iconSelect: false
   };
 
   MetisMenu.prototype.init = function() {
@@ -116,11 +117,23 @@
         .addClass('doubleTapToGo');
     }
 
-    this
-      .$element
-      .find('li')
-      .has('ul')
-      .children('a')
+    if (this.options.iconSelect) {
+        var elm = this
+            .$element
+            .find('li')
+            .has('ul')
+            .children('a')
+            .find('span');
+    } else {
+        var elm = this
+            .$element
+            .find('li')
+            .has('ul')
+            .children('a');
+    }
+
+
+    elm
       .on('click.metisMenu', function(e) {
         var self = $(this);
         var $parent = self.parent('li');
