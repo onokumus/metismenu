ed8358ce0f3261aacf78de101b30c73b0b03340c
QQ <sinabs@gmail.com>
2016-03-08 19:06:18 +0800

    Update metisMenu.js

--- dist/metisMenu.js
+++ dist/metisMenu.js
@@ -148,7 +148,7 @@
         }
 
         if($this.options.onTransitionStart) {
-          $this.options.onTransitionStart();
+          $this.options.onTransitionStart(e);
         }
 
         //Do we need to enable the double tap
