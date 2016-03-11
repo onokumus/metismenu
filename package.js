e722be976517433a03bc01ceaea7861654ec115d
Osman Nuri Okumuş <onokumus@gmail.com>
2016-03-11 17:24:41 +0200

    meteor support

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
   name: 'onokumus:metismenu',
-  version: '2.4.2',
+  version: '2.4.3',
   summary: 'A jQuery menu plugin',
-  git: 'https://github.com/rgnevashev/metisMenu',
+  git: 'https://github.com/onokumus/metisMenu',
   documentation: 'README.md'
 });
 
@@ -10,6 +10,6 @@ Package.onUse(function(api) {
   api.use(['jquery@1.11.4']);
   api.addFiles([
     "dist/metisMenu.css",
-    "dist/metisMenu.js"    
+    "dist/metisMenu.js"
   ], 'client');
 });
