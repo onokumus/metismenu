866576c1e44b3a6ff05d2652884dc374352ac8ec
onokumus <onokumus@gmail.com>
2018-04-20 12:02:28 +0300

    remove browserlist

--- postcss.config.js
+++ postcss.config.js
@@ -17,16 +17,6 @@ module.exports = ctx => ({
       header: banner,
     },
     'autoprefixer': {
-      browsers: [
-        'Android 2.3',
-        'Android >= 4',
-        'Chrome >= 20',
-        'Firefox >= 24',
-        'Explorer >= 8',
-        'iOS >= 6',
-        'Opera >= 12',
-        'Safari >= 6'
-      ],
       cascade: false
     },
     cssnano: ctx.env === 'production' ? {} : false
