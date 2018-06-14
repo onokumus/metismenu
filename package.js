b38d7a86ccf30e1da95f8cd84a42a54925ae3382
onokumus <onokumus@gmail.com>
2018-06-14 03:36:55 +0300

    update keywords

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'onokumus:metismenu',
   version: '2.7.8',
-  summary: 'A jQuery menu plugin',
+  summary: 'A menu plugin',
   git: 'https://github.com/onokumus/metisMenu',
   documentation: 'README.md'
 });
