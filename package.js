8520783183785993985a39122f0e9aeb744299e2
onokumus <onokumus@gmail.com>
2018-10-05 21:21:38 +0300

    v3.0.3

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'onokumus:metismenu',
-  version: '3.0.1',
+  version: '3.0.3',
   summary: 'A jQuery menu plugin',
   git: 'https://github.com/onokumus/metisMenu',
   documentation: 'README.md'
